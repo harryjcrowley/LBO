bbfd88a4005bf03f7593f7c54325a09abf6510e7
harryjcrowley <harrycrowley123@gmail.com>
2026-04-29 20:09:10 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,43 @@
 import streamlit as st
+import openpyxl
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("LBO Converter Tool")
+st.divider()
+uploaded_file = st.file_uploader("Chose a file")
+if uploaded_file is not None:
+    to_add=None
+    import pandas as pd
+    df=pd.read_csv(uploaded_file)
+    new=df[df['CurrentStepName']=='SGA Finance Committee Approval']
+    print(new.head())
+    new=new.drop(columns=['Id',
+                    'ProcessName',
+                    'SubmitterName',
+                    'SubmitterEmail',
+                    'LastSubmitted',
+                    'Status',
+                    'CurrentStepName',
+                    'BudgetName',
+                    'SectionName',
+                    'LineItemName'])
+    new['FirstSubmitted']=new['FirstSubmitted'].str[0:10]
+    ordered=new[['FirstSubmitted', 'SubmittedOnBehalfOf', 'Title', 'RequestedAmount', 'AdjustedAmount']]
+    to_add=st.text_input('Date of committee meeting (MM/DD/YY): ')
+    if len(to_add)==10:
+        num=len(ordered)
+        dates=[]
+        for f in range(num):
+            dates.append(to_add)
+        ordered['FC Date']=dates
+        from io import BytesIO
+        buffer = BytesIO()
+        ordered.to_excel(buffer, sheet_name='Data', index=False)
+        buffer.seek(0)
+
+        st.download_button(
+            label="Download data as excel file",
+            data=buffer.getvalue(),
+            file_name='formatted.xlsx',
+            mime='text/csv',)
+    
+st.badge("v1.0 | Created by Harry Crowley", color='blue')
