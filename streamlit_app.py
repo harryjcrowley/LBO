07c48ce302fd230b87f76a05c7431971f94860e4
harryjcrowley <harrycrowley123@gmail.com>
2026-05-05 02:12:57 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,5 @@ if uploaded_file is not None:
         for f in range(num):
             dates.append(to_add)
         ordered['FC Date']=dates
-        st.table(ordered)   
+        st.table(ordered, width="content", height="content", hinde_index="true")   
 st.badge("v1.0 | Created by Harry Crowley", color='blue')
